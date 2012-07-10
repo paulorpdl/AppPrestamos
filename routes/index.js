2b0d0f05220c6a94011a7dc4cb81a1f766cd5702
Paulo Pinto <paulorpinto@gmail.com>
2012-07-09 21:14:36 -0500

    new file:   app.js

---
+++ routes/index.js
@@ -0,0 +1,35 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+//Express and Node.js Hello World
+
+/*
+ * 
+ * This function returns the classic "Hello World" message on the client and the console.
+ *  
+ * Esta funcion retorna  el clasico mensaje "Hello World" (Hola mundo) al cliente y a la consola.
+*/
+
+exports.hello = function(request,response){
+	
+	//This line defines the Http Header Response
+	//Esta linea define el encabezado de la respuesta HTTP
+	response.writeHead(200,{'Content-type':'text/plain'});
+	
+	//Send the message to the console
+	//Envia el mensaje a la consola
+	//console.log('Hello World\n to %s', response);
+	console.log("Hello World to the %s",request.connection.remoteAddress);
+	
+	
+	//Send the message to the client
+	//Envia el mensaje al cliente
+	response.end('Hello World\n');
+	
+};
